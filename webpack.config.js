c3421dd0143a00d1bf3e5917f58c286c0cf21c10
Sean Durham <sean@sdurham.net>
2017-10-21 08:17:46 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var webpack = require("webpack"),
+    path = require("path"),
+    fileSystem = require("fs"),
+    env = require("./utils/env"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    WriteFilePlugin = require("write-file-webpack-plugin"),
+    CopyWebpackPlugin = require("copy-webpack-plugin");
+
+// load the secrets
+var alias = {};
+
+var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+
+var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
+
+if (fileSystem.existsSync(secretsPath)) {
+  alias["secrets"] = secretsPath;
+}
+
+var options = {
+  entry: {
+    popup: path.join(__dirname, "src", "popup/index.js"),
+    inject: path.join(__dirname, "src", "inject/index.js")
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name]/index.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: [require('babel-plugin-transform-object-rest-spread')]
+          }
+        }
+      },
+    ]
+  },
+  resolve: {
+    alias: alias
+  },
+  plugins: [
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "popup/index.html"),
+      filename: "popup.html",
+      chunks: ["popup"]
+    }),
+    new CopyWebpackPlugin([
+      { context: 'src/', from: '**/*' },
+    ], {
+      ignore: [ '*.js' ]
+    }),
+    new WriteFilePlugin()
+  ]
+};
+
+if (env.NODE_ENV === "development") {
+  options.devtool = "cheap-module-eval-source-map";
+}
+
+module.exports = options;
